ffbab5f102e62b60950319cf564cf6971cfff7cc
Abdul Qawi Kamran <qawitherev@gmail.com>
2025-02-19 22:44:17 +0800

    working on filters and soring param on getUsers

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -1,155 +1,168 @@
 /**
- * @author Abdul Qawi Bin Kamran 
+ * @author Abdul Qawi Bin Kamran
  * @version 0.0.3
  */
 
 class UserRepository {
-    constructor(pool) {
-        this.pool = pool;
-    }
+  constructor(pool) {
+    this.pool = pool;
+  }
 
-    async createUser(userTypeId, username, password, nickname) {
-        const connection = await this.pool.getConnection();
-        try {
-            await connection.beginTransaction();
-            const [result] = await connection.query(
-                `
+  async createUser(userTypeId, username, password, nickname) {
+    const connection = await this.pool.getConnection();
+    try {
+      await connection.beginTransaction();
+      const [result] = await connection.query(
+        `
                 INSERT INTO user (userTypeId, username, password, nickname, createdAt)
                 VALUES (?, ?, ?, ?, NOW())
-                `, 
-                [userTypeId, username, password, nickname]
-            ); 
-            await connection.commit();
-            return result.insertId;
-        } catch (err) { 
-            await connection.rollback();
-            throw err; 
-        } finally {
-            connection.release();
-        }
+                `,
+        [userTypeId, username, password, nickname]
+      );
+      await connection.commit();
+      return result.insertId;
+    } catch (err) {
+      await connection.rollback();
+      throw err;
+    } finally {
+      connection.release();
     }
+  }
 
-    async getUserByUsername(username) {
-        try {
-            const [user] = await this.pool.query(
-                `
+  async getUserByUsername(username) {
+    try {
+      const [user] = await this.pool.query(
+        `
                 SELECT id, username 
                 FROM user 
                 WHERE username = ?
-                `, 
-                [username]
-            );
-            return user[0] || null;
-        } catch {
-            throw err; 
-        }
+                `,
+        [username]
+      );
+      return user[0] || null;
+    } catch {
+      throw err;
     }
+  }
 
-    async createUserType(userType) {
-        const connection = await this.pool.getConnection(); 
+  async createUserType(userType) {
+    const connection = await this.pool.getConnection();
 
-        try {
-            await connection.beginTransaction();
-            const [result] = await connection.query(
-                `
+    try {
+      await connection.beginTransaction();
+      const [result] = await connection.query(
+        `
                 INSERT INTO userType (label, isActive)
                 VALUES (?, 1)
-                `, 
-                [userType]
-            ); 
-            return result.insertId; 
-        } catch (err) {
-            throw err; 
-        }
+                `,
+        [userType]
+      );
+      return result.insertId;
+    } catch (err) {
+      throw err;
     }
+  }
 
-    async getUserTypeId(userType) {
-        try {
-            const [result] = await this.pool.query(
-                `
+  async getUserTypeId(userType) {
+    try {
+      const [result] = await this.pool.query(
+        `
                 SELECT id 
                 FROM userType
                 WHERE label = ? AND 
                 isActive = 1
-                `, 
-                [userType]
-            );
-            return result[0].id || null;
-        } catch (err) {
-            throw err; 
-        }
+                `,
+        [userType]
+      );
+      return result[0].id || null;
+    } catch (err) {
+      throw err;
     }
+  }
 
-    async getUserByUsernameAndPassword(username, hashedPassword) {
-        try {
-            const [result] = await this.pool.query(
-                `
+  async getUserByUsernameAndPassword(username, hashedPassword) {
+    try {
+      const [result] = await this.pool.query(
+        `
                 SELECT id, username, password 
                 FROM user 
                 WHERE username = ? AND 
                 password = ?
-                `, 
-                [username, hashedPassword]
-            )
-            return result[0] || null;
-        } catch (err) {
-            throw err; 
-        }
+                `,
+        [username, hashedPassword]
+      );
+      return result[0] || null;
+    } catch (err) {
+      throw err;
     }
+  }
 
-    async findAllUsersPaged(page = 1, pageSize = 50) {
-        const offset = (page - 1) * pageSize;
-        try {
-            const [result] = await this.pool.query(
-                `
+  async findAllUsersPaged(
+    searchKeyword,
+    userType,
+    startDate,
+    endDate,
+    page = 1,
+    pageSize = 50
+  ) {
+    const offset = (page - 1) * pageSize;
+    try {
+      const [result] = await this.pool.query(
+        `
                 select u.id, ut.label as userType, u.username, u.nickname, u.createdAt from user u 
                 inner join usertype ut on ut.id = u.userTypeId
-                order by u.id desc 
+                where (
+                u.username like concat('%', ?, '%') or
+                u.nickname like concat('%', ?, '%')
+                ) and 
+                u.userTypeId = (select id from usertype ut where ut.label = ?) and 
+                u.createdAt between ? and ?
+                order by u.id desc
                 limit ? offset ?
-                `, 
-                [pageSize, offset]
-            );
-            const [res] = await this.pool.query(
-                `
+                `,
+        [searchKeyword, userType, pageSize, offset]
+      );
+      const [res] = await this.pool.query(
+        `
                 select count(*) as total from user
                 `
-            );
-            const count = res[0].total;
-            return {
-                users: result || [], 
-                pagination: {
-                    currentPage: page, 
-                    pageSize: pageSize, 
-                    totalItems: count, 
-                    totalPages: Math.ceil(count/pageSize)
-                }
-            };
-        } catch (err) {
-            throw err; 
-        }
+      );
+      const count = res[0].total;
+      return {
+        users: result || [],
+        pagination: {
+          currentPage: page,
+          pageSize: pageSize,
+          totalItems: count,
+          totalPages: Math.ceil(count / pageSize),
+        },
+      };
+    } catch (err) {
+      throw err;
     }
+  }
 
-    /**
-     * 
-     * @param {number} userId 
-     * @returns confession | reaction 
-     */
-    async findUserReactions(userId) {
-        try {
-            const [result] = await this.pool.query(
-                `
+  /**
+   *
+   * @param {number} userId
+   * @returns confession | reaction
+   */
+  async findUserReactions(userId) {
+    try {
+      const [result] = await this.pool.query(
+        `
                 select cr.confessionId as confessionId, rt.label as reaction
                 from confessionreaction cr 
                 join reactionType rt on cr.reactionTypeId = rt.id
                 where cr.reactorId = ?
-                `, 
-                [userId]
-            )
-            return result || null;
-        } catch (err) {
-            throw err; 
-        }
+                `,
+        [userId]
+      );
+      return result || null;
+    } catch (err) {
+      throw err;
     }
+  }
 }
 
 module.exports = UserRepository;
